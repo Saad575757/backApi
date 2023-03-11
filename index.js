0e7b6cb25048ad40de7a43fee83b9d91f0300adc
Saad575757 <smartysaad59@gmail.com>
2023-03-11 17:04:45 +0500

    Done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors=require('cors');
 const app=express();
 
 app.use(cors());
-const port= process.env.PORT||3000;
+const port= process.env.PORT||4000;
 const apiData=require('./data..json')
 app.get('/',(req,res)=>{
     res.send('Hey, I am live')
